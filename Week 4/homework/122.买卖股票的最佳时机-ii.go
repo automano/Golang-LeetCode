40b1a320d279f5a92bd9a93ed5cd3416d1d49061
Zhening <18501262663@163.com>
2021-04-23 23:24:45 +0800

    23号每日一题

---
+++ Week 4/homework/122.买卖股票的最佳时机-ii.go
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=122 lang=golang
+ *
+ * [122] 买卖股票的最佳时机 II
+ */
+
+// @lc code=start
+func maxProfit(prices []int) int {
+	result := 0
+	if prices == nil {
+		return result
+	}
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			result += prices[i] - prices[i-1]
+		}
+	}
+	return result
+}
+
+// @lc code=end
+
